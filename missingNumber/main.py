3639c609f4af9446232c27edff11edf4f3da3539
babaksmani <Babak.j.samani@gmail.com>
2018-12-12 18:53:52 -0600

    Implementation of missing number in an array.

--- missingNumber/main.py
+++ missingNumber/main.py
@@ -3,13 +3,21 @@ import random
 
 
 def main():
-    # Create random array with randomly distributed numbers
-    # return np.random.randint(1, 101, 100)
-    rand_index = random.sample(range(0, 100), 1)
+    n = 1000
+    # Create random array of n numbers with randomly distributed order
+    rand_nums = random.sample(range(1, n+1), n)
+    # Generate a random index
+    rand_index = random.sample(range(0, n), 1)
     rand_index = rand_index[0]
-    rand_nums = random.sample(range(1, 101), 100)
-    print rand_nums[rand_index]
-    return sorted(rand_nums, key=int)
+
+    # Now set the number in that index to zero
+    rand_nums[rand_index] = 0
+    total = 0
+    for i in range(0, n):
+        total += rand_nums[i]
+
+    missing = n * (n + 1)/2 - total
+    return missing
 
 
 if __name__ == '__main__':
