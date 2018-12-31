5b9e3208fc264c5544de137030f53c720cf078e9
babaksmani <Babak.j.samani@gmail.com>
2018-12-30 18:34:40 -0600

    Binary Tree implementation

--- insertPlusMinus/insertMinusPlus.py
+++ insertPlusMinus/insertMinusPlus.py
@@ -1,5 +1,7 @@
+import unittest
 
-def main(number, array_list):
+
+def insert_minus_plus(number, array_list):
     total = 0
     inserted_str = ''
     result = False
@@ -16,5 +18,12 @@ def main(number, array_list):
     return result
 
 
+class UnitTest(unittest.TestCase):
+    def insert_test(self):
+        self.assertTrue(insert_minus_plus(2, [1, 4, 3, 2]), True)
+        self.assertTrue(insert_minus_plus(10, [1, 4, 3, 2]), True)
+
+
 if __name__ == "__main__":
-    print main(2, [1, 4, 3, 2])
+    print insert_minus_plus(2, [1, 4, 3, 2])
+    unittest.main()
